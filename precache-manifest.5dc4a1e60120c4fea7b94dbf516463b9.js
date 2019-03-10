7da872e8bca36dd4d5e24da35787ee992b9ce618
Craig Palermo <craig.palermo@opmbx.org>
2019-03-10 13:04:59 -0400

    Updates

---
+++ precache-manifest.5dc4a1e60120c4fea7b94dbf516463b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3361d03f2c94a64e8be8",
+    "url": "/slider-perf-demo/static/js/runtime~main.a98c9a1b.js"
+  },
+  {
+    "revision": "519bcf60485f92900776",
+    "url": "/slider-perf-demo/static/js/main.23dd16be.chunk.js"
+  },
+  {
+    "revision": "9b9bdf9f91a74abf05f7",
+    "url": "/slider-perf-demo/static/js/2.e7f9f0fe.chunk.js"
+  },
+  {
+    "revision": "519bcf60485f92900776",
+    "url": "/slider-perf-demo/static/css/main.be5ead27.chunk.css"
+  },
+  {
+    "revision": "9b9bdf9f91a74abf05f7",
+    "url": "/slider-perf-demo/static/css/2.781af80f.chunk.css"
+  },
+  {
+    "revision": "217a5789bdc7b3dddbd5cf06b421e730",
+    "url": "/slider-perf-demo/index.html"
+  }
+];
